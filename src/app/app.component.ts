4e3730744b765ed613dd580bf1b7fb676c6beca8
IAmBaconator <107735348+IAmBaconator@users.noreply.github.com>
2025-10-20 23:01:29 +0000

    Created header module, linked to app, and img.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { AppHeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
+  imports: [AppHeaderComponent],
 })
 export class AppComponent {}
