7bca33f73e4a4c75ed80767b60203dbca321bcdf
IAmBaconator <107735348+IAmBaconator@users.noreply.github.com>
2025-10-21 16:38:19 +0000

    Link User Input Component to Application.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 import { AppHeaderComponent } from './header/header.component';
+import { UserInputComponent } from './user-input/user-input.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [AppHeaderComponent],
+  imports: [AppHeaderComponent, UserInputComponent],
 })
 export class AppComponent {}
