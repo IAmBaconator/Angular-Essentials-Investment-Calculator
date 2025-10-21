8572193cc8e6224047972ae7e722e54ce3075902
IAmBaconator <107735348+IAmBaconator@users.noreply.github.com>
2025-10-21 17:17:39 +0000

    Setup two-way binding via FormsModel properties

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -9,7 +9,17 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+  //Properties
+  enteredInitialInvestment = '0';//setup without using signals.
+  enteredAnnualInvestiment = '0';
+  enteredExpectedReturn = '5';
+  enteredDuration = '10';
+
   onSubmit() {
     console.log('Submitted!!'); //Verify function is working properly.
+    console.log(this.enteredInitialInvestment);
+    console.log(this.enteredAnnualInvestiment);
+    console.log(this.enteredExpectedReturn);
+    console.log(this.enteredDuration);
   }
 }
